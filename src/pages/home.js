673bc42a8b349c359d81f78b246f5f0089fc4584
aichipeng <1262428635@qq.com>
2019-02-21 17:26:42 +0800

    ...

--- src/pages/home.js
+++ src/pages/home.js
@@ -9,13 +9,15 @@ export default class Home extends React.Component {
     // };
     navigateTo(path) {
         console.log(path)
+        this.props.history.push(path)
     }
     render() {
         return (
             <div>
                 <h1>首页</h1>
                 <button onClick={() => this.props.history.push('user')}>个人中心</button><br/>
-                <button onClick={() => this.props.history.push('list')}>列表</button>
+                <button onClick={() => this.props.history.push('list')}>列表</button><br/>
+                <button onClick={() => this.navigateTo('list')}>函数跳转列表</button>
             </div>
         )
     }
